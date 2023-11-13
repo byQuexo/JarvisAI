bd0bcae8b3267bc49573fc49186a7d7501d97fac
byQuexo <66476537+byQuexo@users.noreply.github.com>
2023-11-13 23:58:06 +0100

    Jarvis Projekt 1.0

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+
+from flask import Flask, render_template, request
+import torchaudio
+from main import main
+#python text to speech
+import pyttsx3
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template('index.html')
+
+@app.route('/audio', methods=['GET', 'POST'])
+def get_text():
+    audio = request.files.get('audio')
+    temp_filename = 'temp_audio.flac'
+    audio.save(temp_filename)
+    res = main(temp_filename)
+    os.remove(temp_filename)
+    return res
+
+if __name__ == '__main__':
+    app.run()
---
+++ main.py
@@ -0,0 +1,36 @@
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+import json
+
+
+def main(audio):
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+    model_id = "openai/whisper-large-v3"
+
+    model = AutoModelForSpeechSeq2Seq.from_pretrained(
+        model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+    )
+    model.to(device)
+
+    processor = AutoProcessor.from_pretrained(model_id)
+
+    pipe = pipeline(
+        "automatic-speech-recognition",
+        model=model,
+        tokenizer=processor.tokenizer,
+        feature_extractor=processor.feature_extractor,
+        max_new_tokens=128,
+        chunk_length_s=30,
+        batch_size=16,
+        return_timestamps=True,
+        torch_dtype=torch_dtype,
+        device=device,
+    )
+
+    result = result = pipe(audio, return_timestamps=True, generate_kwargs={"language": "german", "task": "translate"})
+    res = result["text"]
+    # make json object
+    res = json.dumps({"text": res})
+    return res
